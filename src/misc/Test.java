7e3789fbf29cb156afe32c6fe285b55486942f35
Tor Inge Brekka <tor_ib@hotmail.com>
2021-01-14 13:36:15 +0100

    lagde setup filer og testklasse

---
+++ src/misc/Test.java
@@ -0,0 +1,12 @@
+package misc;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Test {
+
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
